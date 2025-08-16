41edd470b9176d89289cbd1add9c58d221fbca83
Andrew Brampton <github@bramp.net>
2025-08-16 10:23:12 -0700

    Updated the TrackingNumber API to allow invalid TrackNumbers to be returned, as well as returning a list of possible candidates. This helps in the situation where we may not be able to identify the exact courier from the number, and could possibly use some external data source to make the final determination.

--- tracking_numbers/__init__.py
+++ tracking_numbers/__init__.py
@@ -14,13 +14,34 @@ else:
     DEFINITIONS = []
 
 
-def get_tracking_number(number: str) -> Optional[TrackingNumber]:
+def get_tracking_number(number: str, validate=True) -> Optional[TrackingNumber]:
+    """Returns a TrackingNumber that matches the given number.
+
+    Args:
+        number (str): The tracking number to match.
+        validate (bool, optional): Whether to validate the tracking number (e.g
+        checksum and other rules). Defaults to True.
+
+    Returns:
+        Optional[TrackingNumber]: The matching TrackingNumber, or None if no match is found.
+    """
     for tn_definition in DEFINITIONS:
         tracking_number = tn_definition.test(number)
-        if tracking_number and tracking_number.valid:
+        if tracking_number and (not validate or tracking_number.valid):
             return tracking_number
 
     return None
+
+
+def possible_tracking_number(number: str) -> List[TrackingNumber]:
+    """Returns a list of TrackingNumbers that match the given number."""
+    possible_numbers = []
+    for tn_definition in DEFINITIONS:
+        tracking_number = tn_definition.test(number)
+        if tracking_number:
+            possible_numbers.append(tracking_number)
+
+    return possible_numbers
 
 
 def get_definition(product_name: str) -> Optional[TrackingNumberDefinition]:
